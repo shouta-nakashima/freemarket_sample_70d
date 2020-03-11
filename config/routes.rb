6949866131345df9c2eb4598ea544877d67d6040
mahiro0614 <60678657+mahiro0614@users.noreply.github.com>
2020-03-11 18:42:04 +0900

    コッミト１
    root.rbに記述してコントローラー作成、新規登録、マイページ、のビューファイルを作った
    トップページから新規登録画面へ遷移できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'users#index'
-  resources :users, only: [:index]
+  resources :users, only: [:index, :new, :create, :destroy, :show] do
+    resources :cards, only: [:index, :new, :create, :destroy, :edit, :update]
+    resources :plofiles, only: [:new, :create]
+    resources :destinations, only: [:edit, :update]
+  end
+  resources :items, only: [:index, :new, :create, :destroy, :show,:edit]
 end
