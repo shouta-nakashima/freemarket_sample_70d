3fb2af9252db88de4f5fee689eeaf3b61a760e3d
坂上計介 <iwayasunset@gmail.com>
2020-03-15 12:14:39 +0900

    ItemsCreateeSettin
    アイテム出品(new)ページに、フォームを仮設し、createアクションPOSTメソッドでレコード保存するかテストしました
    （ローカルではできました）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,40 +3,24 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
   def create
     Item.create(item_params)
-    redirect_to root_path
   end
-
   def edit
     @item = Item.find(params[:id])
   end
-  
-
   def show
-    
     @item = Item.find(params[:id])
   end
-
-
   def update
     @item = Item.find(params[:id])
   end
-
-
-
   def destroy
     item = Item.find(params[:id])
     item.destroy
   end
-
-
   private
   def item_params
-    params.require(:item).permit(:name, :introduction, :price, :prefecture_code, :item_images_id, :category_id, :brand_id, :item_condition_id, :preparation_day_id, :postage_payer_id, :size_id, :seller_id, :buyer_id)
+    params.require(:item).permit(:name, :introduction, :price,:prefecture_code,:item_images_id,:category_id,:brand_id,:item_condition_id,:preparation_day_id,:postage_payer_id,:size_id,:seller_id,:buyer_id)
   end
-
-
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  # allメソッドではidが無いと怒られるのでとりあえずfindメソッド配列１指定
   def index
     @item = Item.find(1)
   end
