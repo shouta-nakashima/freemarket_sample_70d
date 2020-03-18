bd4133964598d813fdb4e04da02527cb4be50903
mahiro0614 <60678657+mahiro0614@users.noreply.github.com>
2020-03-18 19:25:33 +0900

    commit_new

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'home#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get 'destinations', to: 'users/registrations#new_destination'
+    post 'destinations', to: 'users/registrations#create_destination'
+  end
+  root to: "home#index"
   resources :users, only: [:index, :new, :create, :destroy, :show] do
-    resources :cards, only: [:index, :new, :create, :destroy, :edit, :update]
+    resources:cards, only: [:index, :new, :create, :destroy, :edit, :update]
   end
   resources :items, only: [:new, :create, :destroy, :show, :edit, :update]
 end
