ddd88ae7ad149a52e70283db301fc2fb29ef4938
mahiro0614 <60678657+mahiro0614@users.noreply.github.com>
2020-03-20 12:11:28 +0900

    Revert "commit1"

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,7 +1,6 @@
 class Destination < ApplicationRecord
   belongs_to :user, optional: true
-  validates :d_first_name, :d_family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :d_first_name_kana, :d_family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :d_first_name, :d_family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー一-龠]+\z/i }
+  validates :d_first_name_kana, :d_family_name_kana, presence: true, format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/i }
   validates :post_code,:prefecture_code,:city,:house_number, presence: true
-  validates :post_code, format: { with: /\A\d{7}\z/}
 end
