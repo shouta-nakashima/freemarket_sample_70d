707c4875c4a36e1258e017f9199856e439d7564b
takuya0607 <60646178+takuya0607@users.noreply.github.com>
2020-03-22 13:49:46 +0900

    test_category

---
+++ app/assets/javascripts/category.js
@@ -0,0 +1,81 @@
+$(function(){
+  function appendOption(category){
+    var html = `<option value="${category.id}" data-category="${category.id}">${category.name}</option>`;
+    return html;
+  }
+  function appendChidrenBox(insertHTML){
+    var childSelectHtml = '';
+    childSelectHtml = `<div class='sell__about__right__wrap-box select' id= 'children_wrapper'>
+                          <select class="listing-select-wrapper__box--select" id="child_category" name="item[category_id]">
+                            <option value="---" data-category="---">---</option>
+                            ${insertHTML}
+                          <select>
+                          <i class='fas fa-chevron-down listing-select-wrapper__box--arrow-down'></i>
+                      </div>`;
+    $('.form_label).append(childSelectHtml);
+  }
+ 
+  $('#parent_category').on('change', function(){
+    var parentCategory = document.getElementById('parent_category').value;
+    if (parentCategory != "選択してください"){ 
+      $.ajax({
+        url: '/items/get_category_children',
+        type: 'GET',
+        data: { parent_name: parentCategory },
+        dataType: 'json'
+      })
+      .done(function(children){
+        $('#children_wrapper').remove(); 
+        $('#grandchildren_wrapper').remove();
+        var insertHTML = '';
+        children.forEach(function(child){
+          insertHTML += appendOption(child);
+        });
+        appendChidrenBox(insertHTML);
+      })
+      .fail(function(){
+        alert('カテゴリー取得に失敗しました');
+      })
+    }else{
+      $('#children_wrapper').remove(); 
+      $('#grandchildren_wrapper').remove();
+    }
+  });
+  function appendGrandchidrenBox(insertHTML){
+    var grandchildSelectHtml = '';
+    grandchildSelectHtml = `<div class='sell__about__right__wrap-box select' id= 'grandchildren_wrapper'>
+                                <select class="listing-select-wrapper__box--select" id="grandchild_category" name="product[category_id]">
+                                  <option value="---" data-category="---">---</option>
+                                  ${insertHTML}
+                                </select>
+                                <i class='fas fa-chevron-down listing-select-wrapper__box--arrow-down'></i>
+                            </div>`;
+    $('.sell__about__right__wraps').append(grandchildSelectHtml);
+  }
+  $('.sell__about__right__wraps').on('change', '#child_category', function(){
+    var childId = $('#child_category option:selected').data('category'); 
+    if (childId != "---"){ 
+      $.ajax({
+        url: '/items/get_category_grandchildren',
+        type: 'GET',
+        data: { child_id: childId },
+        dataType: 'json'
+      })
+      .done(function(grandchildren){
+        if (grandchildren.length != 0) {
+          $('#grandchildren_wrapper').remove(); 
+          var insertHTML = '';
+          grandchildren.forEach(function(grandchild){
+            insertHTML += appendOption(grandchild);
+          });
+          appendGrandchidrenBox(insertHTML);
+        }
+      })
+      .fail(function(){
+        alert('カテゴリー取得に失敗しました');
+      })
+    }else{
+      $('#grandchildren_wrapper').remove();
+    }
+  });
+})
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,24 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @category_parent_array = ["選択してください"]
+    Category.where(ancestry: nil).each do |parent|
+    @category_parent_array << parent.name
+    end
+  end
+  def get_category_children
+    @category_children = Category.find_by(name: params[:parent_name]).children
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
   def create
     Item.create(item_params)
+    @category_parent_array = ["選択してください"]
+    Category.where(ancestry: nil).each do |parent|
+    @category_parent_array << parent.name
+    end
   end
   def edit
     @item = Item.find(params[:id])
