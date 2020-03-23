a7fcc52458f74fa5827263cd56d0af36c9f22ab2
坂上計介 <iwayasunset@gmail.com>
2020-03-23 12:27:16 +0900

    Update seeds.rb
    初期のItemデモデータ記述を削除しました

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,5 @@
 # coding: utf-8
 
-Item.create!(
-  id: 1,
-  item_images_id: 1,
-  name: '勇者のTシャツ',
-  introduction:'これは綿のTシャツです',
-  category_id: 1,
-  brand: 'ノーブランド',
-  item_condition_id:1,
-  postage_payer_id: 1,
-  prefecture_code: 1,
-  preparation_day_id: 1,
-  price:3980,
-  seller_id: 1,
-  buyer_id: 1,
-  )
 
 # レディース
 lady = Category.create(name: "レディース")
